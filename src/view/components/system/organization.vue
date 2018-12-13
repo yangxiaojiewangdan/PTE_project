028705af4636ed48b8eae8886b3a092973b25589
yanzijian <l>
2018-12-13 11:56:45 +0800

    弹框

--- src/view/components/system/organization.vue
+++ src/view/components/system/organization.vue
@@ -24,7 +24,7 @@
                     <!-- 表格上面的功能 -->
                         <Col span="24">
                             <div class="organization">
-                                <Button  type="success" class="organization_tableTop">添加</Button>
+                                <Button @click="AddDepartment = true"  type="success" class="organization_tableTop">添加</Button>
                                 <Button @click="delete1 = true"  type="error" class="organization_tableTop">删除</Button>
                                 <Select v-model="model1" style="width:100px" class="organization_tableTop">
                                     <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
@@ -52,6 +52,53 @@
                     <h2>确定删除此数据？</h2>
             </Modal>
         <!-- 删除信息弹出框 end-->
+        <!-- 添加信息 弹出框-->
+            <Modal v-model="AddDepartment" width="700" title="添加部门信息">
+               
+                <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80">
+                    <FormItem label="部门代码" prop="Code">
+                        <Input v-model="formValidate.Code" placeholder="请输入"></Input>
+                    </FormItem>
+                    <FormItem label="部门名称" prop="Description">
+                        <Input v-model="formValidate.Description" placeholder="请输入"></Input>
+                    </FormItem>
+                    <FormItem label="部门描述" prop="LongDescription">
+                        <Input v-model="formValidate.LongDescription" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="请输入"></Input>
+                    </FormItem>
+                    <FormItem label="主管姓名" prop="Supervisor">
+                        <Select v-model="formValidate.Supervisor" style="width:400px" placeholder="请选择">
+                            <OptionGroup label="Hot Cities">
+                                <Option v-for="item in cityList1" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                            </OptionGroup>
+                            <OptionGroup label="Other Cities">
+                                <Option v-for="item in cityList2" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                            </OptionGroup>
+                        </Select>
+                    </FormItem>
+                    <FormItem label="启用" prop="Enabled" >
+                        <i-switch v-model="formValidate.Enabled" size="large" >
+                            <span slot="open">On</span>
+                            <span slot="close">Off</span>
+                        </i-switch>
+                    </FormItem>
+                    <FormItem label="排序码" prop="SortKey">
+                        <Input v-model="formValidate.SortKey" placeholder="请输入"></Input>
+                    </FormItem>
+                </Form>
+                <div  slot="footer">
+                    <div style="margin-left: 20px;float: left;">
+                        <span>创建人:闫子健</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>创建时间:2018/12/12 10：28：00</span><br>
+                        <span>更新人:闫子健</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>更新时间:2018/12/12 10：28：00</span>
+                    </div>
+                    <button type="button" class="ivu-btn ivu-btn-primary ivu-btn-large" @click="handleSubmit('formValidate');">
+                        <span>确定</span>
+                    </button> 
+                    <button type="error" class="ivu-btn ivu-btn-text ivu-btn-large" @click="handleReset('formValidate');AddDepartment = false;">
+                        <span>取消</span>
+                    </button>
+                </div>
+            </Modal>
+        <!-- 添加信息 弹出框 end-->
     </div>
 </template>
 <script>
@@ -289,19 +336,121 @@
                 // 表格 end      
                 // 删除信息弹出框
                     delete1: false,
-                // 删除信息弹出框 end     
+                // 删除信息弹出框 end   
+                // 添加信息 弹出框
+                    cityList: [
+                        {
+                            value: 'New York',
+                            label: 'New York'
+                        },
+                        {
+                            value: 'London',
+                            label: 'London'
+                        },
+                        {
+                            value: 'Sydney',
+                            label: 'Sydney'
+                        },
+                        {
+                            value: 'Ottawa',
+                            label: 'Ottawa'
+                        },
+                        {
+                            value: 'Paris',
+                            label: 'Paris'
+                        },
+                        {
+                            value: 'Canberra',
+                            label: 'Canberra'
+                        }
+                    ],
+                    cityList1: [
+                        {
+                            value: 'New York',
+                            label: 'New York'
+                        },
+                        {
+                            value: 'London',
+                            label: 'London'
+                        },
+                        {
+                            value: 'Sydney',
+                            label: 'Sydney'
+                        }
+                    ],
+                    cityList2: [
+                        {
+                            value: 'Ottawa',
+                            label: 'Ottawa'
+                        },
+                        {
+                            value: 'Paris',
+                            label: 'Paris'
+                        },
+                        {
+                            value: 'Canberra',
+                            label: 'Canberra'
+                        }
+                    ],
+                    Supervisor: '',
+                    AddDepartment: false,
+                    formValidate: {
+                        Enabled: true,
+                        Code: '',
+                        Description: '',
+                        LongDescription: '',
+                        Supervisor: '',
+                        SortKey: '',
+                    },
+                    ruleValidate: {
+                        Code: [
+                            { required: true, message: '部门代码不能为空', trigger: 'blur' },
+                            { min: 8, max: 8, message: "长度必须是8位字符", trigger: "blur" },
+                            { pattern:/^[0-9a-zA-Z]*$/g,message: "必须是字母加数值", trigger: "blur" }
+                        ],
+                        Description: [
+                            { required: true, message: '部门名称部能为空!', trigger: 'blur' },
+                        ],
+                        LongDescription: [
+                            { required: true, message: '部门描述部能为空', trigger: 'blur' }
+                        ],
+                        Supervisor: [
+                            { required: true, message: '请选择主管姓名', trigger: 'change' }
+                        ],
+                        SortKey: [
+                            { required: true, message: '请输入排序码', trigger: 'change' },
+                            {type: 'number', message:'必须是数字', trigger:'blur'}
+                        ],
+                    }
+                // 添加信息 弹出框 end  
             }
         },
         methods: {
-            // 删除信息函数
+            // 删除信息 弹出框函数
                 ok () {
                     this.$Message.info('已删除');
                 },
                 cancel () {
                     this.$Message.info('已取消');
                 },
-            // 删除信息函数 end
-
+            // 删除信息 弹出框函数 end
+            // 添加信息 弹出框函数
+                handleSubmit (name) {
+                    this.$refs[name].validate((valid) => {
+                        if (valid) {
+                            this.$Message.success('成功!');
+                            this.AddDepartment = false;
+                        } else {
+                            this.$Message.error('请输入正确的格式!');
+                        }
+                    })
+                },
+                handleReset (name) {
+                    
+                    this.$refs[name].resetFields();
+                    this.$Message.info('已取消添加部门');
+                }
+            // 添加信息 弹出框函数 end
         }
     }
 </script>
