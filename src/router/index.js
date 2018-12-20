1237f49c858650e638003808279bf9c2fab05f3c
yangxiaojie <1149265110@qq.com>
2018-12-20 16:46:11 +0800

    杨 登陆必须刷新

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ import config from '@/config'
 const { homeName } = config
 
 Vue.use(Router)
-const router = new Router ({
+const router = new Router({
   routes,
   mode: 'history'
 })
@@ -35,19 +35,20 @@ router.beforeEach((to, from, next) => {
       name: homeName // 跳转到homeName页
     })
   } else {
-    if (store.state.user.hasGetInfo) {
-      turnTo(to, store.state.user.access, next)
-    } else {
-      store.dispatch('getUserInfo').then(user => {
-        // 拉取用户信息，通过用户权限和跳转的页面的name来判断是否有权限访问;access必须是一个数组，如：['super_admin'] ['super_admin', 'admin']
-        turnTo(to, user.access, next)
-      }).catch(() => {
-        settoken('')
-        next({
-          name: 'login'
-        })
-      })
-    }
+  	 turnTo(to, store.state.user.access, next)
+//  if (store.state.user.hasGetInfo) {
+//    turnTo(to, store.state.user.access, next)
+//  } else {
+//    store.dispatch('getUserInfo').then(user => {
+//      // 拉取用户信息，通过用户权限和跳转的页面的name来判断是否有权限访问;access必须是一个数组，如：['super_admin'] ['super_admin', 'admin']
+//      turnTo(to, user.access, next)
+//    }).catch(() => {
+//      settoken('')
+//      next({
+//        name: 'login'
+//      })
+//    })
+//  }
   }
 })
 
