666a8c032b96d091d0a477db3c46c6f6fff9019c
yanzijian <l>
2018-12-24 18:13:47 +0800

    修改用户信息

--- src/api/data.js
+++ src/api/data.js
@@ -6,53 +6,51 @@ export const apiUrl = 'http://192.168.31.74:8081'
 //获取人员信息表格  多个数据
 export const getTableData = data4 => {
   return axios.request({
-    url: apiUrl+'/api/BusinessUser/GetEntities',
+    url: apiUrl + '/api/BusinessUser/GetEntities',
     method: 'post',
-    data:data4
+    data: data4
   })
 }
 //添加人员信息接口
-export const uploadMessage= formData => {
+export const uploadMessage = formData => {
   return axios.request({
-    url: apiUrl+'/api/BusinessUser/Create',
+    url: apiUrl + '/api/BusinessUser/Create',
     data: formData,
     method: 'post'
   })
 }
 //修改人员信息接口
-export const UpdateUserData= formData => {
+export const UpdateUserData = formData => {
   return axios.request({
-    url: apiUrl+'/api/BusinessUser/Update',
+    url: apiUrl + '/api/BusinessUser/Update',
     data: formData,
-    method: 'post'  
+    method: 'post'
   })
 }
 
 
 //添加组织信息接口
 export const addBusinessUnit = formData => {
-	return axios.request({
-		url: apiUrl + '/api/BusinessUnit/Create',
-		data: formData,
-		method: 'post'
-	})
+  return axios.request({
+    url: apiUrl + '/api/BusinessUnit/Create',
+    data: formData,
+    method: 'post'
+  })
 }
 //删除组织信息接口多个
 export const deleteBusinessUnit = delBusinessUnitList => {
-	return axios.request({
-		url: apiUrl + '/api/BusinessUnit/DeleteRange',
-		data: delBusinessUnitList,
-		method: 'post'
-	})
+  return axios.request({
+    url: apiUrl + '/api/BusinessUnit/DeleteRange',
+    data: delBusinessUnitList,
+    method: 'post'
+  })
 }
 
 //查询组织接口
 export const BusinessUnitGetEntities = BusinessUnitQuery => {
-	return axios.request({
-		url: apiUrl + '/api/BusinessUnit/GetEntities',
-		data: BusinessUnitQuery,
-		method: 'post'
-	})
+  return axios.request({
+    url: apiUrl + '/api/BusinessUnit/GetEntities',
+    data: BusinessUnitQuery,
+    method: 'post'
+  })
 }
-
-
